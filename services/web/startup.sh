54e946cbba01ebf7128cf9652ca38d6de62acb6c
gschandok <gschandok07@gmail.com>
2020-07-28 12:11:53 +0530

    updated start script

--- services/web/startup.sh
+++ services/web/startup.sh
@@ -2,16 +2,19 @@
 
 echo "Inside startup.sh file"
 
-if [ -z "${AGENT_VERSION}" ]; then
-  echo "installing appdynamics"
-  pip install -U --default-timeout=1000 appdynamics || exit $?
+if [ "${APPDYNAMICS_ENABLED}" = "true" ]; then
+  if [ -z "${AGENT_VERSION}" ]; then
+    echo "installing appdynamics"
+    pip install -U --default-timeout=1000 appdynamics || exit $?
+  else
+    pip install -U appdynamics==${AGENT_VERSION} || exit $?
+  fi
+  # start app
+  echo "starting server with appdynamics monitoring"
+  pyagent run -c appdynamics.cfg - gunicorn -w 4 -b 0.0.0.0:5000 manage:app
 else
-  pip install -U appdynamics==${AGENT_VERSION} || exit $?
+  gunicorn -w 4 -b 0.0.0.0:5000 manage:app
 fi
-
-# start app
-pyagent run -c appdynamics.cfg - gunicorn -w 4 -b 0.0.0.0:5000 manage:app
-#gunicorn -w 4 -b 0.0.0.0:5000 manage:app
 
 # init db
 flask db init
